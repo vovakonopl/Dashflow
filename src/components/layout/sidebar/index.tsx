6164b13e0574b17f56a838b0007a0eee7167c258
Volodymyr Konopl <konopl538@gmail.com>
2025-08-10 14:08:35 +0300

    feat: add semantics for side navbar.

--- src/components/layout/sidebar/index.tsx
+++ src/components/layout/sidebar/index.tsx
@@ -22,78 +22,82 @@ import {
 
 const SideNavbar = async () => {
   return (
-    <SidebarProvider className="max-md:bg-sidebar text-sidebar-foreground max-md:min-h-8 md:w-[var(--sidebar-width)]">
-      <SidebarTrigger className="ml-2 box-content py-1 md:hidden" />
+    <aside>
+      <nav>
+        <SidebarProvider className="max-md:bg-sidebar text-sidebar-foreground max-md:min-h-8 md:w-[var(--sidebar-width)]">
+          <SidebarTrigger className="ml-2 box-content py-1 md:hidden" />
 
-      <Sidebar collapsible="icon">
-        <SidebarTrigger className="mt-1 ml-2 box-content md:hidden" />
-        <SidebarContent>
-          <SidebarGroup>
-            <SidebarMenu>
-              <SidebarMenuItem>
-                <ActiveSidebarMenuButton asChild href="/">
-                  {/* Dashboard */}
-                  <Link href="/">
-                    <LayoutDashboard />
-                    <span>Dashboard</span>
-                  </Link>
-                </ActiveSidebarMenuButton>
-              </SidebarMenuItem>
+          <Sidebar collapsible="icon">
+            <SidebarTrigger className="mt-1 ml-2 box-content md:hidden" />
+            <SidebarContent>
+              <SidebarGroup>
+                <SidebarMenu>
+                  <SidebarMenuItem>
+                    <ActiveSidebarMenuButton asChild href="/">
+                      {/* Dashboard */}
+                      <Link href="/">
+                        <LayoutDashboard />
+                        <span>Dashboard</span>
+                      </Link>
+                    </ActiveSidebarMenuButton>
+                  </SidebarMenuItem>
 
-              <SidebarMenuItem>
-                <ActiveSidebarMenuButton asChild href="/projects">
-                  <Link href="/projects">
-                    <FolderOpen />
-                    <span>Projects</span>
-                  </Link>
-                </ActiveSidebarMenuButton>
-              </SidebarMenuItem>
+                  <SidebarMenuItem>
+                    <ActiveSidebarMenuButton asChild href="/projects">
+                      <Link href="/projects">
+                        <FolderOpen />
+                        <span>Projects</span>
+                      </Link>
+                    </ActiveSidebarMenuButton>
+                  </SidebarMenuItem>
 
-              <SidebarMenuItem>
-                <ActiveSidebarMenuButton asChild href="/tasks">
-                  <Link href="/tasks">
-                    <ClipboardList />
-                    <span>Tasks</span>
-                  </Link>
-                </ActiveSidebarMenuButton>
-              </SidebarMenuItem>
-            </SidebarMenu>
-          </SidebarGroup>
-        </SidebarContent>
+                  <SidebarMenuItem>
+                    <ActiveSidebarMenuButton asChild href="/tasks">
+                      <Link href="/tasks">
+                        <ClipboardList />
+                        <span>Tasks</span>
+                      </Link>
+                    </ActiveSidebarMenuButton>
+                  </SidebarMenuItem>
+                </SidebarMenu>
+              </SidebarGroup>
+            </SidebarContent>
 
-        {/* TODO: display 'Log In' button instead of whole group while user is signed out */}
-        <SidebarFooter>
-          <SidebarGroup>
-            <SidebarMenu>
-              <SidebarMenuItem>
-                <ActiveSidebarMenuButton asChild href="/settings">
-                  <Link href="/settings">
-                    <Settings />
-                    <span>Settings</span>
-                  </Link>
-                </ActiveSidebarMenuButton>
-              </SidebarMenuItem>
+            {/* TODO: display 'Log In' button instead of whole group while user is signed out */}
+            <SidebarFooter>
+              <SidebarGroup>
+                <SidebarMenu>
+                  <SidebarMenuItem>
+                    <ActiveSidebarMenuButton asChild href="/settings">
+                      <Link href="/settings">
+                        <Settings />
+                        <span>Settings</span>
+                      </Link>
+                    </ActiveSidebarMenuButton>
+                  </SidebarMenuItem>
 
-              <SidebarMenuItem>
-                <ActiveSidebarMenuButton asChild href="/profile">
-                  <Link href="/profile">
-                    <Users />
-                    <span>Profile</span>
-                  </Link>
-                </ActiveSidebarMenuButton>
-              </SidebarMenuItem>
+                  <SidebarMenuItem>
+                    <ActiveSidebarMenuButton asChild href="/profile">
+                      <Link href="/profile">
+                        <Users />
+                        <span>Profile</span>
+                      </Link>
+                    </ActiveSidebarMenuButton>
+                  </SidebarMenuItem>
 
-              <SidebarMenuItem>
-                <SidebarMenuButton className="cursor-pointer">
-                  <LogOut />
-                  <span>Log Out</span>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-            </SidebarMenu>
-          </SidebarGroup>
-        </SidebarFooter>
-      </Sidebar>
-    </SidebarProvider>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton className="cursor-pointer">
+                      <LogOut />
+                      <span>Log Out</span>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                </SidebarMenu>
+              </SidebarGroup>
+            </SidebarFooter>
+          </Sidebar>
+        </SidebarProvider>
+      </nav>
+    </aside>
   );
 };
 
