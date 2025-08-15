98ed208149628cdca44796878c7a1879e2ad6d4f
Volodymyr Konopl <konopl538@gmail.com>
2025-08-15 14:39:43 +0300

    feat: add middleware with all routes protected by default.

---
+++ src/middleware.ts
@@ -0,0 +1,38 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { getSession } from '@/lib/server/session';
+
+const publicRoutes = ['/', '/sign-in', '/sign-up', '/404'];
+
+// Routes that are allowed only for unauthorized users
+const authRoutes = ['/sign-in', '/sign-up'];
+
+export async function middleware(req: NextRequest) {
+  const currentPath: string = req.nextUrl.pathname;
+
+  // Check whether the current route is protected.
+  // Redirect users without valid session to sign-in page
+  if (!publicRoutes.includes(currentPath)) {
+    const session = await getSession();
+    if (!session) {
+      return NextResponse.redirect(new URL('/sign-in', req.nextUrl));
+    }
+
+    // Render protected route
+    return NextResponse.next();
+  }
+
+  // Check whether the user is on the authentication pages.
+  // If they are already authorised, redirect them to the dashboard page.
+  if (authRoutes.includes(currentPath)) {
+    const session = await getSession();
+    if (session) {
+      return NextResponse.redirect(new URL('/dashboard', req.nextUrl));
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+};
