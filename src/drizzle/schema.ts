1c3e9351c0777f6ac8934f1e6a0b4b57b7dd97c5
Volodymyr Konopl <konopl538@gmail.com>
2025-08-17 20:28:55 +0300

    feat: add new tables and relations in drizzle schema.

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,4 +1,7 @@
+import { relations } from 'drizzle-orm';
 import {
+  boolean,
+  pgEnum,
   pgTable,
   text,
   timestamp,
@@ -6,17 +9,116 @@ import {
   uuid,
 } from 'drizzle-orm/pg-core';
 
+// Enums
+export const projectRoleEnum = pgEnum('project_role', ['leader', 'member']);
+export const taskPriorityEnum = pgEnum('task_priority', [
+  'low',
+  'medium',
+  'high',
+  'critical',
+]);
+
+// Users
 export const users = pgTable(
   'users',
   {
     id: uuid('id').primaryKey().defaultRandom(),
     email: text('email').notNull(),
-    firstName: text('firstName').notNull(),
-    lastName: text('lastName').notNull(),
+    firstName: text('first_name').notNull(),
+    lastName: text('last_name').notNull(),
     password: text('password').notNull(),
     createdAt: timestamp('created_at').defaultNow().notNull(),
   },
   (table) => ({
-    emailIndex: uniqueIndex('emailIndex').on(table.email),
+    emailIndex: uniqueIndex('email_index').on(table.email),
+  }),
+);
+
+// Projects
+export const projects = pgTable('projects', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  name: text('name').notNull(),
+  description: text('description'),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+
+  ownerId: uuid('owner_id')
+    .notNull()
+    .references(() => users.id, { onDelete: 'cascade' }),
+});
+
+export const projectMembers = pgTable('project_members', {
+  projectId: uuid('project_id')
+    .notNull()
+    .references(() => projects.id, { onDelete: 'cascade' }),
+  userId: uuid('user_id')
+    .notNull()
+    .references(() => users.id, { onDelete: 'cascade' }),
+  role: projectRoleEnum('role').notNull(),
+});
+
+// Tasks
+export const tasks = pgTable('tasks', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  title: text('title').notNull(),
+  description: text('description'),
+  deadline: timestamp('deadline').notNull(),
+  priority: taskPriorityEnum('priority').notNull(),
+  completed: boolean('completed').notNull().default(false),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+
+  projectId: uuid('project_id')
+    .notNull()
+    .references(() => projects.id, { onDelete: 'cascade' }),
+});
+
+export const taskAssignments = pgTable('task_assignments', {
+  taskId: uuid('task_id')
+    .notNull()
+    .references(() => tasks.id, { onDelete: 'cascade' }),
+  userId: uuid('user_id')
+    .notNull()
+    .references(() => users.id, { onDelete: 'cascade' }),
+});
+
+// Relations
+export const projectsRelations = relations(projects, ({ one, many }) => ({
+  owner: one(users, {
+    fields: [projects.ownerId],
+    references: [users.id],
+  }),
+  members: many(projectMembers),
+  tasks: many(tasks),
+}));
+
+export const projectMembersRelations = relations(projectMembers, ({ one }) => ({
+  project: one(projects, {
+    fields: [projectMembers.projectId],
+    references: [projects.id],
+  }),
+  user: one(users, {
+    fields: [projectMembers.userId],
+    references: [users.id],
+  }),
+}));
+
+export const tasksRelations = relations(tasks, ({ one, many }) => ({
+  project: one(projects, {
+    fields: [tasks.projectId],
+    references: [projects.id],
+  }),
+  assignedMembers: many(taskAssignments),
+}));
+
+export const taskAssignmentsRelations = relations(
+  taskAssignments,
+  ({ one }) => ({
+    task: one(tasks, {
+      fields: [taskAssignments.taskId],
+      references: [tasks.id],
+    }),
+    user: one(users, {
+      fields: [taskAssignments.userId],
+      references: [users.id],
+    }),
   }),
 );
