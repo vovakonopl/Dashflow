921c32c1e351a9fb915aaaf1d3d0bef742cc48df
Volodymyr Konopl <konopl538@gmail.com>
2025-08-25 16:23:13 +0300

    refactor: enhance the user type; add object for db select method.

--- src/lib/types/user.ts
+++ src/lib/types/user.ts
@@ -1,10 +1,13 @@
-export type TUser = {
-  id: string;
-  email: string;
-  firstName: string;
-  lastName: string;
-};
+import { InferSelectModel } from 'drizzle-orm';
+import { users } from '@/drizzle/schema';
 
+type TUserDb = InferSelectModel<typeof users>;
+type TIncludedKeys = 'id' | 'email' | 'firstName' | 'lastName';
+
+// Type for client user objects
+export type TUser = Pick<TUserDb, TIncludedKeys>;
+
+// Object with all included fields for db query
 type TIncludedColumns = { [Key in keyof TUser]: true };
 export const DB_USER_INCLUDED_COLUMNS: Readonly<TIncludedColumns> =
   Object.freeze({
@@ -12,4 +15,14 @@ export const DB_USER_INCLUDED_COLUMNS: Readonly<TIncludedColumns> =
     email: true,
     firstName: true,
     lastName: true,
+  });
+
+// Object with all included fields for db query with select method
+type TSelectedColumns = Pick<typeof users, TIncludedKeys>;
+export const DB_USER_SELECTED_COLUMNS: Readonly<TSelectedColumns> =
+  Object.freeze({
+    id: users.id,
+    email: users.email,
+    firstName: users.firstName,
+    lastName: users.lastName,
   });
