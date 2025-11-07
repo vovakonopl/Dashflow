1dc1c2fec9df052ea04bf04facc898da892077ca
Volodymyr Konopl <konopl538@gmail.com>
2025-11-07 16:02:28 +0200

    fix: remove redundant context provider from TasksTable

--- src/app/projects/[projectId]/_components/table/TasksTable.tsx
+++ src/app/projects/[projectId]/_components/table/TasksTable.tsx
@@ -10,7 +10,6 @@ import {
   TableRow,
 } from '@/components/ui/table';
 import { TTask } from '@/lib/types/tables/task';
-import { PaginationProvider } from './pagination/pagination-context';
 
 type TTasksTableProps = {
   tasks: TTask[];
@@ -18,36 +17,32 @@ type TTasksTableProps = {
 
 const TasksTable = ({ tasks }: TTasksTableProps) => {
   return (
-    <PaginationProvider>
-      <ScrollArea className="w-1 flex-1 pb-4">
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Task Name</TableHead>
-              <TableHead>Status</TableHead>
-              <TableHead>Due Date</TableHead>
-              <TableHead className="text-right">Priority</TableHead>
-            </TableRow>
-          </TableHeader>
+    <ScrollArea className="w-1 flex-1 pb-4">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Task Name</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Due Date</TableHead>
+            <TableHead className="text-right">Priority</TableHead>
+          </TableRow>
+        </TableHeader>
 
-          <TableBody className="text-muted-foreground">
-            <BreakRowsOnPages>
-              {tasks.map((task) => (
-                <TaskRow task={task} key={task.id} />
-              ))}
-            </BreakRowsOnPages>
-          </TableBody>
+        <TableBody className="text-muted-foreground">
+          <BreakRowsOnPages>
+            {tasks.map((task) => (
+              <TaskRow task={task} key={task.id} />
+            ))}
+          </BreakRowsOnPages>
+        </TableBody>
 
-          {tasks.length === 0 && (
-            <TableCaption className="text-base italic">
-              No tasks yet
-            </TableCaption>
-          )}
-        </Table>
+        {tasks.length === 0 && (
+          <TableCaption className="text-base italic">No tasks yet</TableCaption>
+        )}
+      </Table>
 
-        <ScrollBar orientation="horizontal" className="w-full" />
-      </ScrollArea>
-    </PaginationProvider>
+      <ScrollBar orientation="horizontal" className="w-full" />
+    </ScrollArea>
   );
 };
 
