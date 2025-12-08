3e3c3b42008fd88844d654822e03806f3e99bad3
Volodymyr Konopl <konopl538@gmail.com>
2025-12-09 00:12:53 +0200

    feat: add more localizations

--- src/lib/constants/app-locales.ts
+++ src/lib/constants/app-locales.ts
@@ -1,12 +1,12 @@
 import { TLocale } from '@/lib/types/locales';
 
 export const APP_LOCALES = [
-  {
-    locale: 'ua',
-    countryCode: 'ua',
-  },
-  {
-    locale: 'en',
-    countryCode: 'gb',
-  },
+  { locale: 'en', countryCode: 'gb' },
+  { locale: 'ua', countryCode: 'ua' },
+  { locale: 'de', countryCode: 'de' },
+  { locale: 'fr', countryCode: 'fr' },
+  { locale: 'pl', countryCode: 'pl' },
+  { locale: 'ja', countryCode: 'jp' },
+  { locale: 'zh', countryCode: 'cn' },
+  { locale: 'ar', countryCode: 'sa' },
 ] as const satisfies Readonly<TLocale[]>;
